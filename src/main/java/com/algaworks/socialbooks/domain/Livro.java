f18f16b1102a70c0eb0e5e603dfd650c3437c734
Cristiano Lopes <lcristianoprogrammer@gmail.com>
2018-01-12 16:16:24 -0200

    primeiro commit

---
+++ src/main/java/com/algaworks/socialbooks/domain/Livro.java
@@ -0,0 +1,90 @@
+package com.algaworks.socialbooks.domain;
+
+import java.util.Date;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+public class Livro {
+
+	private Long id;
+	private String nome;
+	private Date publicacao;
+	private String editora;
+	private String resumo;
+	private List<Comentario> comentarios;
+	private String autor;
+
+	public Livro() {
+
+	}
+
+	public Livro(String nome) {
+		this.nome = nome;
+	}
+
+	@JsonInclude(Include.NON_NULL)
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@JsonInclude(Include.NON_NULL)
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@JsonInclude(Include.NON_NULL)
+	public Date getPublicacao() {
+		return publicacao;
+	}
+
+	public void setPublicacao(Date publicacao) {
+		this.publicacao = publicacao;
+	}
+
+	@JsonInclude(Include.NON_NULL)
+	public String getEditora() {
+		return editora;
+	}
+
+	public void setEditora(String editora) {
+		this.editora = editora;
+	}
+
+	@JsonInclude(Include.NON_NULL)
+	public String getResumo() {
+		return resumo;
+	}
+
+	public void setResumo(String resumo) {
+		this.resumo = resumo;
+	}
+
+	@JsonInclude(Include.NON_NULL)
+	public List<Comentario> getComentarios() {
+		return comentarios;
+	}
+
+	public void setComentarios(List<Comentario> comentarios) {
+		this.comentarios = comentarios;
+	}
+
+	@JsonInclude(Include.NON_NULL)
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+}
